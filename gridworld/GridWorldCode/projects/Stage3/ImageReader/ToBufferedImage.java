427af34aaa57f0bcfb3b1e2f4e69f91d77151724
Ernie1 <870949712@qq.com>
2018-04-29 09:04:55 +0800

    copy the BMPSaver

--- gridworld/GridWorldCode/projects/Stage3/ImageReader/ToBufferedImage.java
+++ gridworld/GridWorldCode/projects/Stage3/ImageReader/ToBufferedImage.java
@@ -1,3 +1,21 @@
+/* 
+ * AP(r) Computer Science GridWorld Case Study:
+ * Copyright(c) 2005-2006 Cay S. Horstmann (http://horstmann.com)
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * @author Cay Horstmann
+ * @author Chris Nevison
+ * @author Barbara Cloud Wells
+ */
+
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
@@ -17,7 +35,7 @@ public class ToBufferedImage {
 	    }
 	
 	    // Create a buffered image with transparency
-	    BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+	    BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_RGB);
 	
 	    // Draw the image on to the buffered image
 	    Graphics2D bGr = bimage.createGraphics();
